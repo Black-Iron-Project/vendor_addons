77556e696f7e558f5af8f665f25315f5fa551861
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-07 17:09:50 +0800

    addons: Add NothingOS 3.0 clock fonts
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -141,6 +141,8 @@ PRODUCT_PACKAGES += \
     ClockFontIOSOverlay \
     ClockFontHerculesOverlay \
     ClockFontSlimOverlay \
+    ClockFontNtype82Overlay \
+    ClockFontSubwayOverlay \
     FontAccuratistOverlay \
     FontAclonicaOverlay \
     FontAmaranteOverlay \
