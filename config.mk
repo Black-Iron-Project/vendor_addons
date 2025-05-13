d0913188726b442deba5e420651098c2ea71b1e5
ralf979 <ralf979@gmail.com>
2025-05-13 16:49:40 +0200

    addons: dont build SysteUI Clock Weather

--- config.mk
+++ config.mk
@@ -49,8 +49,7 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
-    SystemUIClocks-NumOverlap \
-    SystemUIClocks-Weather
+    SystemUIClocks-NumOverlap
 
 # Fonts
 PRODUCT_PACKAGES += \
