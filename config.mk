7c0b11f55a3ddac10bf19e09aacb82f326d56e03
Ghosuto <clash.raja10@gmail.com>
2025-08-01 12:22:17 +0000

    Add AOD wallpaper overlay
    Change-Id: I438d982515c5d8663e94ecf27c3ae61e2241b6f6

--- config.mk
+++ config.mk
@@ -417,6 +417,10 @@ PRODUCT_PACKAGES += \
     SmartSpaceOffset \
     HideClock
 
+# Aod wallpaper
+PRODUCT_PACKAGES += \
+    AODWALL
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
